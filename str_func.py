68caefd1e70fdcdceb0cd22bf9189eb665b367df
petrova_juliana <juliana6k6@gmail.com>
2023-09-17 15:33:46 +0300

    func return_capitalize, dogstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
+"""
+Функция делает всё заглавными буквами
+"""
 def return_title(words):
     return words.title()
+
+"""
+Функция делает каждое слово с заглавной буквы
+"""
+def capitalize(words):
+    return words.capitalize()
